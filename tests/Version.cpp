e6a7dc8f055009c571dc54ca9692433fbb9ca783
DNKpp <dnkpp2011@gmail.com>
2025-05-14 23:34:09 +0200

    test: add tests for version macros

--- tests/Version.cpp
+++ tests/Version.cpp
@@ -1,2 +1,23 @@
+//          Copyright Dominic (DNKpp) Koepke 2025 - 2025.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #include "ctnp/config/Version.hpp"
 
+TEST_CASE(
+	"Version macros are set accordingly.",
+	"[config]")
+{
+	constexpr int major{CTNP_VERSION_MAJOR};
+	constexpr int minor{CTNP_VERSION_MINOR};
+	constexpr int patch{CTNP_VERSION_PATCH};
+	constexpr char const* full{CTNP_VERSION};
+
+	STATIC_CHECK(0 <= major);
+	STATIC_CHECK(0 <= minor);
+	STATIC_CHECK(0 <= patch);
+	CHECK_THAT(
+		full,
+		Catch::Matchers::Equals(mimicpp::format::format("{}.{}.{}", major, minor, patch)));
+}
