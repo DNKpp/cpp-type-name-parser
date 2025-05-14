06e0590106f6cd466f9295fd8532e6e9e18f138e
DNKpp <dnkpp2011@gmail.com>
2025-05-15 00:09:14 +0200

    feat: add util::projected_value_t backport

---
+++ include/ctnp/C++Backports.hpp
@@ -0,0 +1,32 @@
+//          Copyright Dominic (DNKpp) Koepke 2024 - 2025.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef CTNP_CXX_BACKPORTS_HPP
+#define CTNP_CXX_BACKPORTS_HPP
+
+#pragma once
+
+#include <concepts>
+#include <iterator>
+#include <type_traits>
+
+#include "ctnp/config/Config.hpp"
+
+namespace ctnp::util
+{
+    /**
+     * \brief The alias template projected_value_t obtains the value type by stripping any reference and its topmost
+     * cv-qualifiers of the result type of applying Proj to `std::iter_value_t<I>&`.
+     * \tparam I an indirectly readable type.
+     * \tparam Projection projection applied to an lvalue reference to value type of `I`.
+     * \see https://en.cppreference.com/w/cpp/iterator/projected_value_t
+     * \note Implementation directly taken from https://en.cppreference.com/w/cpp/iterator/projected_value_t
+     */
+    template <std::indirectly_readable I, std::indirectly_regular_unary_invocable<I> Projection>
+    using projected_value_t = std::remove_cvref_t<
+        std::invoke_result_t<Projection&, std::iter_value_t<I>&>>;
+}
+
+#endif
